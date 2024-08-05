c89265cdc58e2b32ffa9bf888e287a2ad7e09849
lju <veryveryeasily@gmail.com>
2024-08-05 14:54:47 -0700

    feat(text): basic prototype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
-import Image from "next/image";
+"use client"
+
+import { useEffect, useState } from "react";
+
+const CLASSES = ["one", "two"];
+const TEXT = "Luke Underwood";
+
+const getRandomClass = () => {
+  return CLASSES[Math.floor(Math.random() * CLASSES.length)];
+}
+
+const styleize = (text: string) => {
+  return text.split("").map((c, idx) => {
+    return <span key={idx} className={getRandomClass()}>{c}</span>
+  });
+}
 
 export default function Home() {
+  const [content, setContent] = useState(() => styleize(TEXT));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setContent(styleize(TEXT));
+    }, 100);
+
+    return () => clearInterval(interval);
+  });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Luke Underwood</p>
+      <p className="text-3xl">{content}</p>
     </main>
   );
 }
