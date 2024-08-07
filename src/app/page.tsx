daffdad644dd49e450c9abc832e4170030c2dcae
lju <veryveryeasily@gmail.com>
2024-08-07 01:08:50 -0700

    style: lime green links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,31 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start gap-4 p-24">
       <h1 className="text-base">
-        <Text>{TEXT}</Text>
+        <Link href="/" className="text-lime-500 underline">
+          <Text>{TEXT}</Text>
+        </Link>
       </h1>
 
       <div className="flex w-full max-w-screen-sm flex-col gap-1.5">
-        <h2 className="mb-2 border-b border-white text-base">
-          <Text>Sections</Text>
+        <h2 className="mb-1 border-b border-white text-2xl">
+          <Text>Index</Text>
         </h2>
 
-        <div className="text-base">
+        <div className="flex flex-col gap-1 text-base">
           <h3 className="text-xs">
-            <Link href="/work" className="underline">
+            <Link href="/art" className="text-lime-500 underline">
+              Art
+            </Link>
+          </h3>
+
+          <h3 className="text-xs">
+            <Link href="/music" className="text-lime-500 underline">
+              Music
+            </Link>
+          </h3>
+
+          <h3 className="text-xs">
+            <Link href="/work" className="text-lime-500 underline">
               Work
             </Link>
           </h3>
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link.js";
 import Text from "../text";
 
 const TEXT = "Luke Underwood";
@@ -6,21 +7,25 @@ export default function Work() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start gap-4 p-24">
       <h1 className="text-base">
-        <Text>{TEXT}</Text>
+        <Link href="/" className="text-lime-500 underline">
+          <Text>{TEXT}</Text>
+        </Link>
       </h1>
 
       <div className="flex w-full max-w-screen-sm flex-col gap-1.5">
-        <h2 className="border-b border-white text-xs">
+        <h2 className="mb-1 border-b border-white text-2xl">
           <Text>Work Experience</Text>
         </h2>
 
-        <div className="text-base">
+        <div className="text-sm">
           <h3>
             <Text>Whitney Museum of American Art</Text>
           </h3>
+
           <h3>
             <Text>Chatlands Horizons</Text>
           </h3>
+
           <h3>
             <Text>The Markup</Text>
           </h3>
