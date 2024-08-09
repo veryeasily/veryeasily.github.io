bdd6b0d33a71c3aae3b7b1219d886ae74889b2de
lju <veryveryeasily@gmail.com>
2024-08-08 20:30:36 -0700

    feat(deploy): try chatgpt's ghpages idea

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // This will make sure Next.js exports a static site.
+  images: {
+    unoptimized: true, // Disable image optimization (GitHub Pages doesn't support Next.js image optimization)
+  },
+  basePath: "/itsliver", // Use this basePath if your site will be hosted under /itsliver
+};
 
 export default nextConfig;
