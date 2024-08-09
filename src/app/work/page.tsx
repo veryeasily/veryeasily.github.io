46f259cdf75a2664cf56c29bc33d6d8116bd5bac
lju <veryveryeasily@gmail.com>
2024-08-08 19:00:13 -0700

    fix(text): missed a page of leading

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -5,19 +5,19 @@ const TEXT = "Luke Underwood";
 
 export default function Work() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start gap-4 p-24">
-      <h1 className="text-base">
+    <main className="flex min-h-screen flex-col items-center justify-start gap-4 p-24 text-base">
+      <h1>
         <Link href="/" className="text-lime-500 underline">
           <Text>{TEXT}</Text>
         </Link>
       </h1>
 
-      <div className="flex w-full max-w-screen-sm flex-col gap-1.5">
-        <h2 className="mb-1 border-b border-white text-2xl">
+      <div className="flex w-full max-w-screen-sm flex-col">
+        <h2 className="mb-1 border-b border-white text-xs">
           <Text>Work Experience</Text>
         </h2>
 
-        <div className="text-sm">
+        <div>
           <h3>
             <Text>Whitney Museum of American Art</Text>
           </h3>
