08281dccd644cdfa55b3b932d68837b160274324
lju <veryveryeasily@gmail.com>
2024-08-10 22:12:12 -0700

    style(home): leading + content tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,24 @@ import BasePage from "@/components/base_page";
 export default function HomePage() {
   return (
     <BasePage>
-      <div className="flex flex-col gap-4 text-black">
+      <article className="flex flex-col text-lg text-black">
         <h2 className="text-2xl">hey!!</h2>
-        <p className="text-lg">this is where i plan to put stuff i do!</p>
-        <div className="text-lg">
-          <p>you can see my resume here:</p>
-          <Link
-            href="https://drive.google.com/file/d/15eq-Ec4faLtWqT8hZ68_YsUuEYIuiSae/view?usp=sharing"
-            className="text-blue-500 underline"
-            target="_blank"
-          >
-            link
-          </Link>
-        </div>
-      </div>
+
+        <section className="pt-2 text-base">
+          <p>this is where i put stuff i do!</p>
+
+          <p>
+            you can see my resume{" "}
+            <Link
+              href="https://drive.google.com/file/d/15eq-Ec4faLtWqT8hZ68_YsUuEYIuiSae/view?usp=sharing"
+              className="text-blue-500 underline"
+              target="_blank"
+            >
+              here
+            </Link>
+          </p>
+        </section>
+      </article>
     </BasePage>
   );
 }
