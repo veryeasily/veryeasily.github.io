a1c203dab498f82de26aaa8557b473a05f779259
lju <veryveryeasily@gmail.com>
2024-08-11 01:16:04 -0700

    organize imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 
+import clsx from "clsx";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import clsx from "clsx";
 
 import Header from "@/app/header";
 
