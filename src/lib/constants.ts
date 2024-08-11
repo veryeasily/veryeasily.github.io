22473ac758e5095f1c0138fbb830cd123939a22f
lju <veryveryeasily@gmail.com>
2024-08-11 05:30:19 -0700

    style(art): add another drawing

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,6 +2,7 @@ export const IMG_LIST = [
   // "https://c1.staticflickr.com/6/5338/9288760247_6153e06904_c.jpg",
   "https://live.staticflickr.com/2868/9291549168_3dfc5989e2_k.jpg",
   "https://live.staticflickr.com/7336/9288777013_725a4854f4_b.jpg",
+  "https://live.staticflickr.com/7294/9291557000_1fed7c4ef3_k.jpg",
   // "https://c1.staticflickr.com/4/3741/9288774947_77bc0a0142_h.jpg",
   "https://c1.staticflickr.com/6/5500/9288758335_d9258bf52b_h.jpg",
   "https://c1.staticflickr.com/8/7404/9288761047_22fa63c71b_h.jpg",
