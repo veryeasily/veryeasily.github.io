685d7207155f54111cdb42ad71311bf7a40bb763
lju <veryveryeasily@gmail.com>
2024-08-10 22:06:10 -0700

    style(home): remove "for now"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
         <h2 className="text-2xl">hey!!</h2>
         <p className="text-lg">this is where i plan to put stuff i do!</p>
         <div className="text-lg">
-          <p>for now, you can see my resume here:</p>
+          <p>you can see my resume here:</p>
           <Link
             href="https://drive.google.com/file/d/15eq-Ec4faLtWqT8hZ68_YsUuEYIuiSae/view?usp=sharing"
             className="text-blue-500 underline"
